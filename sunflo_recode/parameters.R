e7a7248801f0af4d79629329ab9db67f25a08ee1
Sarah Elizabeth Stockman <132379749+SEStockman@users.noreply.github.com>
2025-04-17 13:02:44 -0600

    add parameter

--- sunflo_recode/parameters.R
+++ sunflo_recode/parameters.R
@@ -1,1 +1,20 @@
 # load parameters
+
+#' @title Parameters for Sunflower Recode
+#' @description This script loads the parameters for the sunflower recode project.
+#' @import readxl
+
+
+# load required package
+library(readxl)
+
+# set file directory
+file_directory <- "rsunflo R package/inst/doc/files"
+
+# load parameter excel file tab
+parameter_design <- read_excel(file.path(file_directory, "parameterization_default.xlsx"),
+                                sheet = "design_metadata"
+                               )
+
+# convert to data frame
+parameter_design <- as.data.frame(parameter_design)
