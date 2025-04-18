9165e8e2a50bf5052a1308e80419898d0b4e5811
Sarah Elizabeth Stockman <132379749+SEStockman@users.noreply.github.com>
2025-04-18 10:41:37 -0600

    add example main code

--- sunflo_recode/functions_SE.R
+++ sunflo_recode/functions_SE.R
@@ -1,1 +1,69 @@
 # Sarah Elizabeth's Functions
+# Thermal time accumulation
+thermal_time <- function(Tm, Tb) {
+  if (Tm > Tb) {
+    return(Tm - Tb)
+  } else {
+    return(0)
+  }
+}
+
+# Radiation Interception Efficiency (RIE)
+radiation_interception <- function(LAI, k) {
+  return(1 - exp(-k * LAI))
+}
+
+# Radiation Use Efficiency (RUE)
+radiation_use_efficiency <- function(thermal_time, TDF1, TDM0, TDM3, r0, rmax, rd, rmin, a, b) {
+  if (thermal_time < 300) {
+    return(r0)
+  } else if (thermal_time < TDF1) {
+    return(r0 + 2 * (thermal_time - 300) / (TDF1 - 300))
+  } else if (thermal_time < TDM0) {
+    return(rmax)
+  } else if (thermal_time < TDM3) {
+    return(b * (1 - a * exp((thermal_time - TDM3) / (TDM3 - TDM0))))
+  } else {
+    return(0)
+  }
+}
+
+# Harvest Index
+harvest_index <- function(TDF1, TDM0, TDM3, thermal_time) {
+  if (thermal_time < TDF1) {
+    return(0.2)
+  } else if (thermal_time < TDM0) {
+    return(0.4)
+  } else if (thermal_time < TDM3) {
+    return(0.5)
+  } else {
+    return(0.6)
+  }
+}
+
+
+# Crop Biomass Accumulation
+crop_biomass <- function(PAR, RIE, RUE, previous_biomass) {
+  return(previous_biomass + (PAR * RIE * RUE))
+}
+
+# Crop Yeild
+crop_yield <- function(crop_biomass, harvest_index) {
+  return(biomass * harvest_index)
+}
+
+# Water Stress
+water_stress <- function(FTSW, a) {
+  return(-1 + 1 / (1 + exp(a * FTSW)))
+}
+
+# Leaf Area Index (LAI)
+leaf_area_index <- function(sowing_density, plant_leaf_area) {
+  return(sowing_density * plant_leaf_area)
+}
+
+# Root Growth
+root_growth <- function(Tm, RootGrowthRate, RootDepthLimit, current_root_depth) {
+  new_root_depth <- current_root_depth + RootGrowthRate * Tm
+  return(min(new_root_depth, RootDepthLimit))
+}
--- sunflo_recode/main.R
+++ sunflo_recode/main.R
@@ -1,1 +1,45 @@
 # main
+
+# Load parameters
+source("parameters.R")
+
+# Load climate data
+source("climate_data.R")
+climate_data <- load_climate_data("climate_data.csv")
+
+# Load functions
+# source("functions.R")
+
+# Initialize variables
+thermal_time_accumulated <- 0
+biomass <- 0
+root_depth <- 0
+LAI <- 0
+
+# Loop through climate data
+for (day in 1:nrow(climate_data)) {
+  Tm <- (climate_data$TemperatureAirMin[day] + climate_data$TemperatureAirMax[day]) / 2
+  PAR <- climate_data$Radiation[day] * 0.48
+  
+  # Calculate thermal time
+  thermal_time_accumulated <- thermal_time_accumulated + thermal_time(Tm, Tb)
+  
+  # Calculate RIE
+  RIE <- radiation_interception(LAI, k)
+  
+  # Calculate RUE
+  RUE <- radiation_use_efficiency(thermal_time_accumulated, TDF1, TDM0, TDM3, r0, rmax, rd, rmin, a, b)
+  
+  # Update biomass
+  biomass <- crop_biomass(PAR, RIE, RUE, biomass)
+  
+  # Update root depth
+  root_depth <- root_growth(Tm, 0.7, RootDepthLimit, root_depth)
+  
+  # Update LAI (example: assume constant plant leaf area)
+  LAI <- leaf_area_index(7, 0.1)
+}
+
+# Print final results
+cat("Final Biomass:", biomass, "g/m^2\n")
+cat("Final Root Depth:", root_depth, "mm\n")
